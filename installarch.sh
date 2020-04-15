ef41719d284377185bc36cf10376b0667b106560
kw <ant@arch.localdomain>
2020-04-15 18:45:53 +0200

    fix

--- installarch.sh
+++ installarch.sh
@@ -5,37 +5,40 @@
 hostname="arch"
 country="france"
 timezone="Europe/Paris"
+chroot="arch-chroot /mnt "
 
 ping -c 5 archlinux.org || (echo "check your internet connexion."; exit)
 
 #update the mirrors with reflector in installation environment
 pacman -noconfirm -S reflector
-reflector -c $country -l 5 --sort rate --save /etc/pacman.d/mirrorlist
+reflector -c $country --score 5 --save /etc/pacman.d/mirrorlist
 
 pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
 
-ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
-hwclock --systohc
-sed -i "s/#fr_FR.UTF-8/fr_FR.UTF-8/g" /etc/locale.gen
-locale-gen
-echo "LANG=fr_FR.UTF8" > /etc/locale.conf
-echo "KEYMAP=fr-pc" > /etc/vconsole.conf
-echo $hostname > /etc/hostname
+
+ln -sf /mnt/usr/share/zoneinfo/$timezone /mnt/etc/localtime
+$chroot hwclock --systohc
+sed -i "s/#fr_FR.UTF-8/fr_FR.UTF-8/g" /mnt/etc/locale.gen
+$chroot locale-gen
+echo "LANG=fr_FR.UTF8" > /mnt/etc/locale.conf
+echo "KEYMAP=fr-pc" > /mnt/etc/vconsole.conf
+echo $hostname > /mnt/etc/hostname
 echo "127.0.0.1	localhost
 ::1		localhost
-127.0.1.1	"$hostname".localdomain	"$hostname > /etc/hosts
-mkinitcpio -P
-passwd
+127.0.1.1	"$hostname".localdomain	"$hostname > /mnt/etc/hosts
+$chroot mkinitcpio -P
+$chroot passwd
 
 #update the mirrors with reflector
-pacman -noconfirm -S reflector
-reflector -c $country -l 5 --sort rate --save /etc/pacman.d/mirrorlist
+$chroot pacman -noconfirm -S reflector
+$chroot reflector -c $country --score 5 --save /etc/pacman.d/mirrorlist
 
-cd ~
+cd /mnt/root
 curl -O https://raw.githubusercontent.com/antv0/archsetup/master/postinstall.sh
 chmod +x postinstall.sh
 curl -O https://raw.githubusercontent.com/antv0/archsetup/master/installgrub.sh
 chmod +x installgrub.sh
-curl -O https://raw.githubusercontent.com/antv0/archsetup/master/packages.csv
+curl -O https://raw.githubusercontent.com/antv0/archsetup/master/packages.csv"
+
+$chroot
